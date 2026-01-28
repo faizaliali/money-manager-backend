76278aeb07c33686e98fe1ef4dfab12c25360e36
faizaliali <139729561+faizaliali@users.noreply.github.com>
2026-01-28 08:53:52 +0530

    Update SecurityConfig.java

--- moneymanager/src/main/java/in/faizali/moneymanager/config/SecurityConfig.java
+++ moneymanager/src/main/java/in/faizali/moneymanager/config/SecurityConfig.java
@@ -49,11 +49,6 @@ public class SecurityConfig {
         return http.build();
     }
 
-    // @Bean
-    // public UserDetailsService userDetailsService() {
-    //     return appUserDetailsService;
-    // }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
