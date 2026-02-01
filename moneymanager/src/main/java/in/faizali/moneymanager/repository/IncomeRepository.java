0444f560b87805ff1ef7ee3861195bc9a298c46d
faizaliali <139729561+faizaliali@users.noreply.github.com>
2026-02-01 19:14:46 +0530

    Update IncomeRepository.java

--- moneymanager/src/main/java/in/faizali/moneymanager/repository/IncomeRepository.java
+++ moneymanager/src/main/java/in/faizali/moneymanager/repository/IncomeRepository.java
@@ -12,16 +12,16 @@ import java.util.List;
 
 public interface IncomeRepository extends JpaRepository<IncomeEntity, Long> {
 
-    //select * from tbl_incomes where profile_id = ?1 order by date desc
+    
     List<IncomeEntity> findByProfileIdOrderByDateDesc(Long profileId);
 
-    //select * from tbl_incomes where profile_id = ?1 order by date desc limit 5
+
     List<IncomeEntity> findTop5ByProfileIdOrderByDateDesc(Long profileId);
 
     @Query("SELECT SUM(i.amount) FROM IncomeEntity i WHERE i.profile.id = :profileId")
     BigDecimal findTotalExpenseByProfileId(@Param("profileId") Long profileId);
 
-    //select * from tbl_incomes where profile_id = ?1 and date between ?2 and ?3 and name like %?4%
+
     List<IncomeEntity> findByProfileIdAndDateBetweenAndNameContainingIgnoreCase(
             Long profileId,
             LocalDate startDate,
@@ -30,6 +30,6 @@ public interface IncomeRepository extends JpaRepository<IncomeEntity, Long> {
             Sort sort
     );
 
-    //select * from tbl_incomes where profile_id = ?1 and date between ?2 and ?3
+
     List<IncomeEntity> findByProfileIdAndDateBetween(Long profileId, LocalDate startDate, LocalDate endDate);
 }
